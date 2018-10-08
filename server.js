f6de5d58e411050019f17914a406e69b4f3dc2c5
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-10-08 10:58:06 +0200

    setup morgan, mongoose

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
+"use strict";
+
+// create express server instance 
 const express = require('express');
 const app = express();
+
+// setup logging middleware
+const morgan = require('morgan');
+app.use(morgan('common'));
+
+// import mongoose
+const mongoose = require('mongoose');
+// use native ES6 promises with mongoose
+mongoose.Promise = global.Promise;
+
+// run server
+
+function runServer() {
+
+}
+
+// close server
+
+
+// start from the terminal
